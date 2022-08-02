7d79a49d60bfe6cd0fde3ed63a7aa47733ade855
kitmichal <k.michal96@o2.pl>
2022-08-02 22:26:43 +0200

    add images, dark mode button, contact page

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+
+function darkMode() {
+    if(document.getElementById("dark_mode").innerHTML === 'Dark Mode'){
+
+    document.body.style.backgroundColor = "black";
+    document.getElementById("dark_mode").innerHTML = "Light Mode"
+    document.body.style.color = "white";
+    
+    
+  }
+
+  else if(document.getElementById("dark_mode").innerHTML === 'Light Mode'){
+
+    document.body.style.backgroundColor = "blanchedalmond";
+    document.getElementById("dark_mode").innerHTML = "Dark Mode"
+    document.body.style.color = "black";
+    
+  }
+}
+
+function contact() {
+  
+  document.getElementById('aboutMe').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('contactBox').style.display = 'flex';
+}
+
+function main() {
+  document.getElementById('aboutMe').style.display = 'flex';
+  document.getElementById('projects').style.display = 'block';
+  document.getElementById('contactBox').style.display = 'none';
+}
