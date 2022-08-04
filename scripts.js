844006dfdc5496d5d44d1a6c9ecaf5ea55030d0a
kitmichal <k.michal96@o2.pl>
2022-08-04 13:39:43 +0200

    add dark mode

--- scripts.js
+++ scripts.js
@@ -1,21 +1,32 @@
 
 
 function darkMode() {
+  const a = document.querySelectorAll("a");
+
     if(document.getElementById("dark_mode").innerHTML === 'Dark Mode'){
 
     document.body.style.backgroundColor = "black";
-    document.getElementById("dark_mode").innerHTML = "Light Mode"
+    document.getElementById("dark_mode").innerHTML = "Light Mode";
     document.body.style.color = "white";
     
+    
+    for (let i = 0; i < a.length; i++) {
+    a[i].style.backgroundColor = "black";
+    a[i].style.color = 'white';
+}
     
   }
 
   else if(document.getElementById("dark_mode").innerHTML === 'Light Mode'){
 
     document.body.style.backgroundColor = "blanchedalmond";
-    document.getElementById("dark_mode").innerHTML = "Dark Mode"
+    document.getElementById("dark_mode").innerHTML = "Dark Mode";
     document.body.style.color = "black";
     
+    for (let i = 0; i < a.length; i++) {
+      a[i].style.backgroundColor = "blanchedalmond";
+      a[i].style.color = 'black'; 
+    }
   }
 }
 
@@ -24,7 +35,7 @@ function contact() {
   document.getElementById('aboutMe').style.display = 'none';
   document.getElementById('projects').style.display = 'none';
   document.getElementById('contactBox').style.display = 'flex';
-  //document.getElementById('footer').style.bottom = '-100px';
+  
 }
 
 function main() {
@@ -32,3 +43,4 @@ function main() {
   document.getElementById('projects').style.display = 'block';
   document.getElementById('contactBox').style.display = 'none';
 }
+
